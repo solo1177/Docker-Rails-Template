722573a5f69c7674f9b9fdc771ea40ee48197c8b
mitani <nico.programming1011@gmail.com>
2023-09-13 18:32:41 +0900

    変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,9 @@
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import HelloMessage from './HelloMessage.js'
 
+Rails.start()
+Turbolinks.start()
 ActiveStorage.start()
